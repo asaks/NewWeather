bfdbe126f5e6e09f278573f506f19ebe285c2d8e
Alexander Aksenov <aksenovpnz@gmail.com>
2018-04-25 08:22:59 +0300

    Create ApplicationSettings.java

---
+++ app/src/main/java/com/asaks/newweather/ApplicationSettings.java
@@ -0,0 +1,29 @@
+package com.asaks.newweather;
+
+import com.asaks.newweather.Constants;
+
+/**
+ * Класс настроек приложения
+ */
+
+public class ApplicationSettings
+{
+
+    // позиция выбранной единицы измерения температуры
+    private int unitTempPos;
+
+    public ApplicationSettings()
+    {
+        this.unitTempPos = Constants.TEMP_KELVIN;
+    }
+
+    public int getUnitTempPos()
+    {
+        return unitTempPos;
+    }
+
+    public void setUnitTempPos( int unitTempPos )
+    {
+        this.unitTempPos = unitTempPos;
+    }
+}
